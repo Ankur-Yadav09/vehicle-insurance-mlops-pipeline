f3f0c6739981641b88fefe05df85ce4ce17374ed
Ankur-Yadav09 <ankur.yadav@neosoftmail.com>
2025-09-23 18:02:21 +0530

    Added project setup files

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Ankur Yadav",
+    author_email="vy8655@gmail.com",
+    packages=find_packages()
+)
